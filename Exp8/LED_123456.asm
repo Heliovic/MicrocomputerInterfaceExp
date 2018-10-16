b9325330b4ca5db18b706d67bcf300885c50f14f
Heliovic <1005031096@qq.com>
2018-10-16 16:23:45 +0800

    Exp8: LED 123456 loop & LED with INTR

---
+++ Exp8/LED_123456.asm
@@ -0,0 +1,64 @@
+PA_8255 EQU 0600H
+PB_8255 EQU 0602H
+PC_8255 EQU 0604H
+CTR_8255 EQU 0606H
+
+DATA SEGMENT
+    LEDTABLE    DB 03FH
+                DB 006H
+                DB 05BH
+                DB 04FH
+                DB 066H
+                DB 06DH
+                DB 07DH
+                DB 007H
+                DB 07FH
+                DB 06FH
+                DB 077H
+                DB 07CH
+                DB 039H
+                DB 05EH
+                DB 079H
+                DB 071H
+DATA ENDS
+CODE SEGMENT
+    ASSUME CS: CODE, DS: DATA
+START:
+    MOV AX, DATA
+    MOV DS, AX
+    
+    ; 8255 INIT
+    MOV DX, CTR_8255
+    MOV AL, 10000000B
+    OUT DX, AL
+    
+AA1:    
+    LEA BX, LEDTABLE
+    MOV SI, 1
+    MOV CX, 6
+    MOV AL, 11111110B
+AA2:    
+    MOV DX, PA_8255
+    OUT DX, AL
+    
+    PUSH AX
+    MOV DX, PB_8255    
+    MOV AL, [BX + SI]
+    OUT DX, AL
+    CALL DELAY
+    
+    POP AX
+    ROL AL, 1
+    INC SI
+    LOOP AA2
+    JMP AA1
+    
+DELAY:
+    PUSH CX
+    MOV CX, 007FH
+    LOOP $
+    POP CX
+    RET
+    
+CODE ENDS
+    END START
---
+++ Exp8/LED_INTR.asm
@@ -0,0 +1,132 @@
+PA_8255 EQU 0600H
+PB_8255 EQU 0602H
+PC_8255 EQU 0604H
+CTR_8255 EQU 0606H
+ICW1_8259 EQU 0020H
+ICW2_8259 EQU 0021H
+ICW4_8259 EQU 0021H
+OCW1_8259 EQU 0021H
+
+IR6_INTR_ADDR EQU 38H
+
+DATA SEGMENT
+	MAP DB 10111111B
+    LEDTABLE    DB 03FH
+                DB 006H
+                DB 05BH
+                DB 04FH
+                DB 066H
+                DB 06DH
+                DB 07DH
+                DB 007H
+                DB 07FH
+                DB 06FH
+                DB 077H
+                DB 07CH
+                DB 039H
+                DB 05EH
+                DB 079H
+                DB 071H
+DATA ENDS
+CODE SEGMENT
+    ASSUME CS: CODE, DS: DATA
+START:
+    MOV AX, DATA
+    MOV DS, AX
+    
+    ; 8255 INIT
+    MOV DX, CTR_8255
+    MOV AL, 10000000B
+    OUT DX, AL
+    
+    CLI
+	
+	MOV DX, ICW1_8259
+	MOV AL, 00010011B
+	OUT DX, AL
+	
+	MOV DX, ICW2_8259
+	MOV AL, 00001000B
+	OUT DX, AL
+	
+	MOV DX, ICW4_8259
+	MOV AL, 00000111B
+	OUT DX, AL
+	
+	MOV DX, OCW1_8259
+	MOV AL, 00101111B
+	OUT DX, AL
+	
+	STI
+
+	CLI
+	MOV AX, 0
+	MOV ES, AX
+	MOV DI, IR6_INTR_ADDR
+	MOV AX, OFFSET IR6
+	CLD
+	STOSW
+	MOV AX, SEG IR6
+	STOSW
+	STI
+    
+AA1:    
+    LEA BX, LEDTABLE
+    MOV SI, 0
+    MOV AL, 11111110B
+AA2:    
+    MOV DX, PA_8255
+    OUT DX, AL
+    
+    PUSH AX
+    MOV DX, PB_8255    
+    MOV AL, [BX + SI]
+    OUT DX, AL
+    ;CALL DELAY
+    
+    POP AX
+    ROL AL, 1
+    CMP AL, 10111111B
+    JE AA1
+    CMP AL, MAP
+    JE AA1
+    JMP AA2
+    
+DELAY:
+    PUSH CX
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    POP CX
+    RET    
+    
+IR6 PROC FAR
+	;STI
+	PUSH AX
+	ROR MAP, 1
+	CMP MAP, 11111110B
+	MOV AL, MAP
+	JE EXIT
+	POP AX
+	IRET
+IR6 ENDP
+
+EXIT:
+	MOV DX, PA_8255
+	MOV AL, 11111111B
+    OUT DX, AL
+    
+    MOV AH, 4CH
+	INT 21H   
+	    
+CODE ENDS
+    END START
