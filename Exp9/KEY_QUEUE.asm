134c62491c2f86de4b839f5eefc908ffa9aed85c
Heliovic <1005031096@qq.com>
2018-10-19 10:48:45 +0800

    Exp9: Display number key input

---
+++ Exp9/KEY_QUEUE.asm
@@ -0,0 +1,161 @@
+PA_8255 EQU 0600H
+PB_8255 EQU 0602H
+PC_8255 EQU 0604H
+CTR_8255 EQU 0606H
+
+DATA SEGMENT
+    LEDMAP DB 10111111B
+    LEDTABLE    DB 03FH
+                DB 006H
+                DB 05BH
+                DB 04FH
+                DB 066H
+                DB 06DH
+                DB 07DH
+                DB 007H
+                DB 07FH
+                DB 06FH
+                DB 077H
+                DB 07CH
+                DB 039H
+                DB 05EH
+                DB 079H
+                DB 071H
+                
+    KEYTABLE    DB 11101110B    ;0
+                DB 11011110B    ;1
+                DB 10111110B    ;2
+                DB 01111110B    ;3
+                DB 11101101B    ;4
+                DB 11011101B    ;5
+                DB 10111101B    ;6
+                DB 01111101B    ;7
+                DB 11101011B    ;8
+                DB 11011011B    ;9
+                DB 10111011B    ;A
+                DB 01111011B    ;B
+                DB 11100111B    ;C
+                DB 11010111B    ;D
+                DB 10110111B    ;E
+                DB 01110111B    ;F
+DATA ENDS
+CODE SEGMENT
+    ASSUME CS: CODE, DS: DATA
+START:
+    MOV AX, DATA
+    MOV DS, AX
+    
+    ; 8255 INIT
+    MOV DX, CTR_8255
+    MOV AL, 10001001B
+    OUT DX, AL
+    
+    MOV DX, PB_8255
+    MOV AL, 0   ;熄灭
+    OUT DX, AL
+    
+    
+AA1:    
+    MOV DX, PA_8255
+    MOV AL, 00000000B
+    OUT DX, AL
+    
+    MOV DX, PC_8255
+    IN AL, DX
+    AND AL, 00001111B
+    
+    CMP AL, 00001111B
+    JE AA1
+    
+    CALL DELAY  ;消抖
+    
+    MOV DX, PA_8255
+    MOV AL, 00000000B
+    OUT DX, AL
+    
+    MOV DX, PC_8255
+    IN AL, DX
+    AND AL, 00001111B
+    
+    CMP AL, 00001111B
+    JE AA1
+    
+    MOV CX, 4
+    MOV AH, 11111110B
+    
+AA2:
+    MOV DX, PA_8255
+    MOV AL, AH
+    OUT DX, AL
+    
+    MOV DX, PC_8255
+    IN AL, DX
+    
+    AND AL, 00001111B
+    CMP AL, 00001111B
+    
+    JNE AA3 ;已确定了行、列
+    
+    ROL AH, 1
+    LOOP AA2
+    
+AA3:
+    MOV CX, 4
+    SHL AH, CL  ;AH中低4位存的列号移到高4位
+    OR AL, AH   ;AH的行号合并到AL，形成位置码与KEYTABLE对应
+    
+    LEA BX, KEYTABLE
+    
+    MOV SI, 0   ;设置初始键号
+AA4:    
+    CMP AL, [BX + SI]
+    JE AA5
+    INC SI
+    ;CMP SI, 16  ;可能有重键
+    ;JE XXX
+    JMP AA4
+    
+AA5:
+    ROR LEDMAP, 1
+    MOV CL, LEDMAP
+    MOV AX, SI
+    MOV AH, AL
+    CALL DISPLAY    
+    
+    JMP AA1
+    
+DISPLAY:
+    ;CL:LEDMAP, AH:NUMBER
+    PUSH AX
+    PUSH BX
+    PUSH DX
+    
+    MOV DX, PA_8255
+    MOV AL, CL
+    OUT DX, AL
+    
+    LEA BX, LEDTABLE
+    MOV AL, AH
+    AND AH, 0
+    MOV SI, AX
+    
+    MOV AL, [BX + SI]
+    MOV DX, PB_8255
+    OUT DX, AL
+    
+    POP DX
+    POP BX
+    POP AX
+    RET
+    
+DELAY:
+    PUSH CX
+    MOV CX, 0FFFFH
+    LOOP $
+    MOV CX, 0FFFFH
+    LOOP $
+    POP CX
+    RET
+      
+CODE ENDS
+    END START
